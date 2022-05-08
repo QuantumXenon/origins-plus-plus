086ae319cf422de050eb0f373b9a73ef904d85e8
QuantumXenon <edwinza31415@gmail.com>
2022-05-08 17:55:29 +0100

    Merged Forge and Fabric versions

---
+++ src/main/java/com/quantumxenon/origins_plus_plus/main.java
@@ -0,0 +1,8 @@
+package com.quantumxenon.origins_plus_plus;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(main.MODID)
+public final class main {
+	public static final String MODID = "origins_plus_plus";
+}
