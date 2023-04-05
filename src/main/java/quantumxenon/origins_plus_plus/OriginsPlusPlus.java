279070621067e0aaefb4c375088841cc5748d766
QuantumXenon <thequantumxenon@gmail.com>
2023-04-05 11:41:03 +0100

    Remove 1.18.2 login message
    1.18.2 will no longer be supported on Forge unless there is a backport to fix the keybinds issue

--- src/main/java/quantumxenon/origins_plus_plus/OriginsPlusPlus.java
+++ src/main/java/quantumxenon/origins_plus_plus/OriginsPlusPlus.java
@@ -1,11 +1,9 @@
 package quantumxenon.origins_plus_plus;
 
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod("origins_plus_plus")
 public class OriginsPlusPlus {
     public OriginsPlusPlus() {
-        MinecraftForge.EVENT_BUS.register(LoginMessage.class);
     }
 }
