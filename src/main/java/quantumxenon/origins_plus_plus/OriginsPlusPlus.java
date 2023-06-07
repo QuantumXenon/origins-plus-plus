36b6e65aa39103e34bafcd53b5427fa13c6cb148
QuantumXenon <thequantumxenon@gmail.com>
2023-06-07 20:19:14 +0100

    Remove building files
    Also remove loader-specific files

--- src/main/java/quantumxenon/origins_plus_plus/OriginsPlusPlus.java
+++
@@ -1,9 +0,0 @@
-package quantumxenon.origins_plus_plus;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod("origins_plus_plus")
-public class OriginsPlusPlus {
-    public OriginsPlusPlus() {
-    }
-}
